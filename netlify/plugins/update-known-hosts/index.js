417bf73fc6782c09b375ba70ec36c57f3ef3479f
Grupo Alpha <deploy@alpha.grup>
2026-08-28 12:08:40 +0300

    Add build plugin to fix GitHub SSH host key verification

---
+++ netlify/plugins/update-known-hosts/index.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+  onPreBuild: ({ utils }) => {
+    const { execSync } = require('child_process');
+    try {
+      execSync('mkdir -p ~/.ssh && ssh-keyscan github.com >> ~/.ssh/known_hosts', { stdio: 'inherit' });
+      console.log('Added github.com to known_hosts');
+    } catch(e) {
+      console.log('Warning: could not add github.com host key:', e.message);
+    }
+  }
+};
